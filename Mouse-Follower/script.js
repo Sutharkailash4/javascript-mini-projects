70c351e38f1e507f9a3d5174ac0aea4c1fc0df2d
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-02-16 11:21:39 +0530

    mouse-follower

--- Mouse-Follower/script.js
+++ Mouse-Follower/script.js
@@ -1,16 +1,7 @@
-let Mouse_follower = document.querySelector('.mouse-follower');
-let inner_circle = document.querySelector('.inner_box');
-let body = document.body;
+const mouseFollower = document.querySelector('.mouse-follower');
+const innerCircle = document.querySelector('.inner_box');
 
-body.addEventListener('mousemove', (e) => {
-    let X = e.clientX - 15;
-    let Y = e.clientY - 15;
-    Mouse_follower.style.position = 'absolute';
-    Mouse_follower.style.top = `${Y}px`;
-    Mouse_follower.style.left = `${X}px`;
-    let x = e.clientX - 5;
-    let y = e.clientY - 5;
-    inner_circle.style.position = 'absolute';
-    inner_circle.style.top = `${y}px`;
-    inner_circle.style.left = `${x}px`;
-})
+document.body.addEventListener('mousemove', (e) => {
+    mouseFollower.style.transform = `translate(${e.clientX-10}px, ${e.clientY-10}px)`;
+    innerCircle.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
